531108b22b80407191969c512e32077f6844600a
hema2317 <hemamadhav3@gmail.com>
2025-04-20 16:08:51 -0600

    Create app.config.js

---
+++ templates/app.config.js
@@ -0,0 +1,8 @@
+export default {
+  expo: {
+    extra: {
+      API_URL: process.env.API_URL || "https://askdoc-27gp.onrender.com",
+      // Add other frontend-specific vars here
+    },
+  },
+};
